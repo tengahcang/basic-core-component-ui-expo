2f371ea5d9c4b2e9e3569f72fa90d104e83d30d3
tengahcang <cangg522@gmail.com>
2023-10-27 09:22:28 +0700

    ini update 1

---
+++ screens/index.js
@@ -0,0 +1,29 @@
+import LotsOfStyles from "./LotsOFStyles";
+import FixedDimensionsBasics from "./FixedDimensionsBasics";
+import FlexDimensionsBasics from "./FlexDimensionsBasics";
+import PercentageDimensionsBasics from "./PercentageDimensionBasics";
+import FlexBasic from "./FlexBasic";
+import FlexDirectionBasics from "./FlexDirectionBasics";
+import JustifyContentBasics from "./JustifyContentBasics";
+import AlignItemsLayout from "./AlignItemsLayout";
+import WidthHeightBasics from "./WidthHeightBasics";
+import PositionLayout from "./PositionLayout";
+import DisplayAnImageWithStyle from "./DisplayAnimageWithStyle";
+import NativeBaseBasic from "./NativeBaseBasic";
+import GlueStackUIBasic from "./GlueStackBasic";
+
+export {
+    LotsOfStyles,
+    FixedDimensionsBasics,
+    FlexDimensionsBasics,
+    PercentageDimensionsBasics,
+    FlexBasic,
+    FlexDirectionBasics,
+    JustifyContentBasics,
+    AlignItemsLayout,
+    WidthHeightBasics,
+    PositionLayout,
+    DisplayAnImageWithStyle,
+    NativeBaseBasic,
+    GlueStackUIBasic,
+};
